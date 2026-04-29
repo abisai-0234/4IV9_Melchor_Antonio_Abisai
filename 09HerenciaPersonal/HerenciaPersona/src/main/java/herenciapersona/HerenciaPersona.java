6a5d222af23f90dda9126443e9d952f41bb749dd
abisai-0234 <amelchora2401@alumno.ipn.mx>
2026-04-28 20:44:39 -0600

    Add files via upload

---
+++ 09HerenciaPersonal/HerenciaPersona/src/main/java/herenciapersona/HerenciaPersona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package herenciapersona;
+
+/**
+ *
+ * @author Alumno
+ */
+public class HerenciaPersona {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
